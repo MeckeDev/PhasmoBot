caa822e43e3e0448a9263126ece34b53a6eddde6
Mecke Dev <crafted.mecke@gmail.com>
2020-09-28 08:00:24 +0200

    Update config.py

--- Twitch_Bot/hidden/config.py
+++ Twitch_Bot/hidden/config.py
@@ -1,6 +1,6 @@
-TMI_TOKEN = 'oauth:gng1e0rjjjehyflljt9sv6vm33kjft'
-CLIENT_ID = 'fjfqgelkaq1v1s4vlcyfhcfomkqjwp'
-BOT_NICK = 'bot_mecke'
+TMI_TOKEN = 'oauth:ax9kgxruhg4lea8oqso20cbbsbfhwo'
+CLIENT_ID = '5rlr0r8pg4vnh9hl4kc8wvqhv4idkz'
+BOT_NICK = 'PhasmoBot'
 BOT_PREFIX = "$"
 CHANNEL = ["mecke_dev", "mrkthulu"] # "thelevelbest", 
 CHANNEL_ID = "55898523"
